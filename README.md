475d906eb10549eca176ed88a449dbd2863bcc19
Joel Bueno <joeljecksonbueno@gmail.com>
2023-07-11 11:36:42 -0300

    Removido os comentarios do fonte.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Projeto_curso_algoritmo_1
-Este projeto e sobre o curso de algoritmo e logica 1
+# PROJETO CURSO ALGORITMO I
+
+### Este projeto e sobre o curso de algoritmo e logica 1
--- estrutura-enquanto.js
+++ estrutura-enquanto.js
@@ -1,25 +1,3 @@
-/*
-   nome: caractere
-   idade, limite, contador: inteiro
-
-Inicio
-// Seção de Comandos, procedimento, funções, operadores, etc...
-    escreva("Digite a quantidade de vezes que vai ser verificado a idade: ")
-    leia(limite)
-    contador := 0
-    enquanto contador < limite faca
-        escreva("digite o nome da pessoa: ")
-        leia(nome)
-        escreva("digite a idade de ", nome, ": ")
-        leia (idade)
-        se idade > 18 entao
-            escreval("Você é maior de idade!")
-        senao
-            escreval("Você é menor de idade!")
-        fimse
-        contador := contador + 1
-    fimenquanto
-*/
 
 function acaoBotao() {
     var nome, idade, limite, contador
